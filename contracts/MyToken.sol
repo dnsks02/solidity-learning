4d1a03bb35f27e337b19ace8faed14f7de11cc2d
dnsks02 <dnsks02@naver.com>
2026-06-30 18:31:36 +0900

    added approve and transferfromFunction

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,12 +2,17 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
     uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) allowance; 
+
 
     constructor(
         string memory _name,
@@ -18,12 +23,28 @@ contract MyToken {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(_amount * 10 ** uint256(decimals), msg.sender);
+        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
+    }
+
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external{
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
     }
 
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+
+        emit Transfer(address(0), owner, amount);
     }
 
     function transfer(uint256 amount, address to) external {
@@ -31,5 +52,10 @@ contract MyToken {
 
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
     }
 }
+
+
+ 
