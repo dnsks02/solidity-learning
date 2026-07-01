61c505e074f828f1c1166f49cad61de3ac1413e8
dnsks02 <dnsks02@naver.com>
2026-07-02 00:12:51 +0900

    access manager apply to TinyBank

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,43 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract MyToken {
+import "./ManagedAccess.sol";
+
+contract MyToken is ManagedAccess {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed spender, uint256 amount);
 
-    address public owner;
-    address public manager;
     string public name;
     string public symbol;
     uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance; 
-
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(
         string memory _name,
         string memory _symbol,
         uint8 _decimal,
         uint256 _amount
-    ) {
-        owner = msg.sender;
-        manager = msg.sender;
+    ) ManagedAccess(msg.sender, msg.sender) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "You are not authorized");
-        _;
-    }
-
-    modifier onlyManager() {
-        require(msg.sender == manager, "You are not authorized to manage this token");
-        _;
+        _mint(_amount * 10 ** uint256(decimals), msg.sender);
     }
 
     function approve(address spender, uint256 amount) external {
@@ -54,11 +41,11 @@ contract MyToken {
         emit Transfer(from, to, amount);
     }
 
-    function mint(uint256 amount, address to) external onlyManager{
+    function mint(uint256 amount, address to) external onlyManager {
         _mint(amount, to);
     }
 
-    function setManager(address _manager) external onlyOwner{
+    function setManager(address _manager) external onlyOwner {
         manager = _manager;
     }
 
@@ -78,6 +65,3 @@ contract MyToken {
         emit Transfer(msg.sender, to, amount);
     }
 }
-
-
- 
